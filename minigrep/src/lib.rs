83ef40e815b5f6cab7c1fcb653754a104c1a88a6
mantou <xianqiao.wang@huaban.com>
2017-11-27 18:37:13 +0800

    learn iterator

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -29,33 +29,31 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Config {
-        if args.len() < 3 {
-            eprintln!("not enough arguments !!!!!!!!");
-        }
-        let query = if let Some(i) = args.get(1) {i.clone()} else {String::from("the")};
-        let filename = if let Some(i) = args.get(1) {i.clone()} else {String::from("poem.txt")};
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Config {
-            query,
-            filename,
-            case_sensitive
-        }
+        Ok(Config {
+            query, filename, case_sensitive
+        })
     }
 }
 
-pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
