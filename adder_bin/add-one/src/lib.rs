21ab4ff9afcb181c15c1ed535cb700e36966473c
王先硚 <xianqiao.wang@upai.com>
2018-03-21 17:55:10 +0800

    feat: 文档注释和 crate 注释

--- adder_bin/add-one/src/lib.rs
+++ adder_bin/add-one/src/lib.rs
@@ -1,5 +1,18 @@
+//! # My Crate
+//!
+//! 这是对整个 crate 的注释
+
 extern crate rand;
 
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let five = 5;
+///
+/// assert_eq!(6, add_one::add_one(5));
+/// ```
 pub fn add_one(x: i32) -> i32 {
     x + 1
 }
