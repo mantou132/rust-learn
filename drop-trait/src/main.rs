8fc26e0af9c1d175f4715a26cec74649a1b8477a
王先硚 <xianqiao.wang@upai.com>
2018-04-03 16:38:42 +0800

    feat: learn drop trait

---
+++ drop-trait/src/main.rs
@@ -0,0 +1,20 @@
+#[derive(Debug)]
+struct CustomSmartPointer {
+    data: String
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop (&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+fn main() {
+    let _c = CustomSmartPointer {
+        data: String::from("my stuff")
+    };
+    drop(_c);
+    let _d = CustomSmartPointer {
+        data: String::from("other stuff")
+    };
+}
