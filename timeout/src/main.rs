9bb05cc34fde9cfcd5b818258ba7070096ce0dcc
王先硚 <xianqiao.wang@upai.com>
2018-08-09 14:57:01 +0800

    feat: add timeout demo

---
+++ timeout/src/main.rs
@@ -0,0 +1,20 @@
+use std::time::{Duration, SystemTime};
+use std::thread::sleep;
+
+fn main() {
+    let now = SystemTime::now();
+
+    // we sleep for 2 seconds
+    sleep(Duration::new(2, 1));
+    match now.elapsed() {
+        Ok(elapsed) => {
+            // it prints '2'
+            println!("secs: {}", elapsed.as_secs());
+            println!("nanos: {}", elapsed.subsec_nanos());
+        }
+        Err(e) => {
+            // an error occurred!
+            println!("Error: {:?}", e);
+        }
+    }
+}
