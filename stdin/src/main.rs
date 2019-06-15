fce562ab6e6a62bd0e3ca5576a40c14e5ba411a2
王先硚 <709922234@qq.com>
2019-06-15 13:15:21 +0800

    feat: learn RAII

---
+++ stdin/src/main.rs
@@ -0,0 +1,24 @@
+use std::io::prelude::*;
+
+fn main () {
+    println!("What is the quote?");
+    // 如果跟后面的 `lock()` 连起来写` io::stdin()` 创建一个临时值资源就会被释放
+    // 后面的读取就不能工作
+    let stdin = std::io::stdin();
+    let mut lines = stdin.lock().lines();
+    let quote = lines
+        .next()
+        .expect("No input")
+        .expect("Failed to read")
+        .trim()
+        .to_string();
+    println!("Who said it?");
+    let name = lines
+        .next()
+        .expect("No input")
+        .expect("Failed to read")
+        .trim()
+        .to_string();
+    // 要获取所有权才能用 `+` 连接字符串
+    println!("{}", name + " says, \"" + &quote + "\"");
+}
